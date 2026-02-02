88ec060b2ccb35971d3981bf57f9afb62e1fd882
VictorNneli <robjay76@gmail.com>
2026-02-02 11:19:33 +0100

    task manager css added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./navbar.css";
 
-function Navbar({setIsLoggedIn}) {
+function Navbar({ setIsLoggedIn }) {
   const navigate = useNavigate();
 
   function handleLogout() {
     setIsLoggedIn(false);
-    navigate('/');}
+    navigate("/");
+  }
 
-    return (
-    <div style={{
-      display: "flex",
-      gap: 15,
-      padding: 15,
-      background: "#6cca14ff",
-      color: "white"
-    }}>
-      <Link to="/dashboard" style={{ color: "white" }}>Dashboard</Link>
-      <Link to="/taskmanager" style={{ color: "white" }}>Tasks</Link>
-      <Link to="/profile" style={{ color: "white" }}>Profile</Link>
-      <Link to="/settings" style={{ color: "white" }}>Settings</Link>
-      <button
-        onClick={handleLogout}
-        style={{ marginLeft: "auto", cursor: "pointer" }}
-      >
+  return (
+    <nav className="navbar">
+      <div className="nav-links">
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/taskmanager">Tasks</Link>
+        <Link to="/profile">Profile</Link>
+        <Link to="/settings">Settings</Link>
+      </div>
+
+      <button className="logout-btn" onClick={handleLogout}>
         Logout
       </button>
-      </div>
-    );
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
